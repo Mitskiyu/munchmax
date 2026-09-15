8fddf7e94b16a2e4623f7f321f291c941580c9b3
Mitskiyu <75166393+Mitskiyu@users.noreply.github.com>
2026-09-15 17:48:51 +0200

    ref: collapse stages

--- pipeline/main.py
+++ pipeline/main.py
@@ -14,7 +14,7 @@ def run():
     fsq_token = os.environ["FSQ_TOKEN"]
 
     data_dir = Path(__file__).resolve().parent.parent / "data"
-    os.makedirs(data_dir, exist_ok=True)
+    data_dir.mkdir(parents=True, exist_ok=True)
 
     kowloon = [
         "Kowloon City District",
@@ -25,14 +25,13 @@ def run():
     ]
 
     with duckdb.connect() as con:
-        attach_places(con, fsq_token)
-        load_districts(con)
-        parquet = save_places(con, f"{data_dir}/hk")
-        filter_district(con, kowloon, parquet)
+        save_places(con, fsq_token)
+        parquet = filter_restaurants(con, data_dir / "hk.parquet")
+        filter_districts(con, kowloon, parquet)
         # fetch_sources(con, client)
 
 
-def attach_places(con, token):
+def save_places(con, token):
     con.execute(f"""
         INSTALL httpfs;
         LOAD httpfs;
@@ -47,67 +46,84 @@ def attach_places(con, token):
             SECRET iceberg_secret,
             ENDPOINT 'https://catalog.h3-hub.foursquare.com/iceberg'
         );
-        """)
+    """)
 
-
-def load_districts(con):
     con.execute("""
         INSTALL spatial;
         LOAD spatial;
 
-        CREATE OR REPLACE TABLE districts AS SELECT *
+        CREATE OR REPLACE TABLE districts AS
+        SELECT *
         FROM ST_Read('https://www.had.gov.hk/psi/hong-kong-administrative-boundaries/hksar_18_district_boundary.json');
     """)
 
-
-def save_places(con, out):
+    # bbox filters before download
     long_min, lat_min, long_max, lat_max = con.execute("""
-        SELECT ST_XMin(g), ST_YMin(g), ST_XMax(g), ST_YMax(g)
-        FROM (SELECT ST_Extent(ST_Union_Agg(geom)) AS g FROM districts);
+        SELECT
+            ST_XMin(g),
+            ST_YMin(g),
+            ST_XMax(g),
+            ST_YMax(g)
+        FROM (
+            SELECT ST_Extent(ST_Union_Agg(geom)) AS g
+            FROM districts
+        );
     """).fetchone()
 
     con.execute(
         """
-        CREATE OR REPLACE TABLE places AS SELECT *
-        FROM fsq.datasets.places_os p
+        CREATE OR REPLACE TABLE places AS
+        SELECT * EXCLUDE(geom)
+        FROM fsq.datasets.places_os AS p
         WHERE
-            longitude BETWEEN $1 AND $2
-            AND latitude BETWEEN $3 and $4
-            AND ST_Within(ST_Point(p.longitude, p.latitude), (SELECT ST_Union_Agg(geom) FROM districts));
+            p.longitude BETWEEN $1 AND $2
+            AND p.latitude BETWEEN $3 AND $4
+            AND ST_Within(
+                ST_Point(p.longitude, p.latitude),
+                (SELECT ST_Union_Agg(geom) FROM districts)
+            );
         """,
         [long_min, long_max, lat_min, lat_max],
     )
 
-    path = f"{out}.parquet"
-    con.execute(f"""
+
+def filter_restaurants(con, out):
+    con.execute(
+        """
         COPY (
-            SELECT * FROM places
+            SELECT *
+            FROM places
             WHERE len(list_filter(
                 fsq_category_labels,
                 lambda x : starts_with(x, 'Dining and Drinking')
-                AND split_part(x, ' > ', 2) NOT IN (
-                    'Bar', 'Winery', 'Vineyard', 'Brewery', 'Distillery'
-                )
+                    AND split_part(x, ' > ', 2) NOT IN (
+                        'Bar', 'Winery', 'Vineyard', 'Brewery', 'Distillery'
+                    )
             )) > 0
             AND date_closed IS NULL
             AND unresolved_flags IS NULL
-        ) TO '{path}';
-        """)
+        ) TO $1;
+        """,
+        [str(out)],
+    )
 
-    return path
+    return out
 
 
-def filter_district(con, district, parquet):
+def filter_districts(con, districts, parquet):
     con.sql(
         f"""
-        SELECT d.district, COUNT(*) AS num_places
-        FROM '{parquet}' p JOIN districts d
+        SELECT
+            d.district,
+            COUNT(*) AS num_places
+        FROM '{parquet}' AS p
+        JOIN districts AS d
             ON ST_Within(ST_Point(p.longitude, p.latitude), d.geom)
         WHERE list_contains($1, d.district)
         GROUP BY d.district
         ORDER BY num_places DESC;
         """,
-        params=[district],
+        params=[districts],
     ).show()
 
 
