f6f4204a34175373322948bfb351e0062069259e
Mitskiyu <75166393+Mitskiyu@users.noreply.github.com>
2026-09-15 16:55:03 +0200

    feat: save only hk poly

--- pipeline/main.py
+++ pipeline/main.py
@@ -16,20 +16,18 @@ def run():
     data_dir = Path(__file__).resolve().parent.parent / "data"
     os.makedirs(data_dir, exist_ok=True)
 
+    kowloon = [
+        "Kowloon City District",
+        "Kwun Tong District",
+        "Sham Shui Po District",
+        "Wong Tai Sin District",
+        "Yau Tsim Mong District",
+    ]
+
     with duckdb.connect() as con:
         attach_places(con, fsq_token)
-
-        hk = (22.1367222, 22.5683333, 113.8171111, 114.5024444)  # lat, long
-        parquet = save_places(con, hk, f"{data_dir}/hk")
-
-        kowloon = [
-            "Kowloon City District",
-            "Kwun Tong District",
-            "Sham Shui Po District",
-            "Wong Tai Sin District",
-            "Yau Tsim Mong District",
-        ]
-
+        load_districts(con)
+        parquet = save_places(con, f"{data_dir}/hk")
         filter_district(con, kowloon, parquet)
         # fetch_sources(con, client)
 
@@ -52,24 +50,38 @@ def attach_places(con, token):
         """)
 
 
-def save_places(con, bbox, out):
-    lat_min, lat_max, lng_min, lng_max = bbox
+def load_districts(con):
+    con.execute("""
+        INSTALL spatial;
+        LOAD spatial;
+
+        CREATE OR REPLACE TABLE districts AS SELECT *
+        FROM ST_Read('https://www.had.gov.hk/psi/hong-kong-administrative-boundaries/hksar_18_district_boundary.json');
+    """)
+
+
+def save_places(con, out):
+    long_min, lat_min, long_max, lat_max = con.execute("""
+        SELECT ST_XMin(g), ST_YMin(g), ST_XMax(g), ST_YMax(g)
+        FROM (SELECT ST_Extent(ST_Union_Agg(geom)) AS g FROM districts);
+    """).fetchone()
+
     con.execute(
         """
         CREATE OR REPLACE TABLE places AS SELECT *
-        FROM fsq.datasets.places_os
-        WHERE 
-            latitude BETWEEN $1 AND $2
-            AND longitude BETWEEN $3 AND $4
+        FROM fsq.datasets.places_os p
+        WHERE
+            longitude BETWEEN $1 AND $2
+            AND latitude BETWEEN $3 and $4
+            AND ST_Within(ST_Point(p.longitude, p.latitude), (SELECT ST_Union_Agg(geom) FROM districts));
         """,
-        [lat_min, lat_max, lng_min, lng_max],
+        [long_min, long_max, lat_min, lat_max],
     )
 
     path = f"{out}.parquet"
     con.execute(f"""
         COPY (
-            SELECT * EXCLUDE(geom)
-            FROM places
+            SELECT * FROM places
             WHERE len(list_filter(
                 fsq_category_labels,
                 lambda x : starts_with(x, 'Dining and Drinking')
@@ -86,14 +98,6 @@ def save_places(con, bbox, out):
 
 
 def filter_district(con, district, parquet):
-    con.execute("""
-        INSTALL spatial;
-        LOAD spatial;
-
-        CREATE OR REPLACE TABLE districts AS SELECT *
-        FROM ST_Read('https://www.had.gov.hk/psi/hong-kong-administrative-boundaries/hksar_18_district_boundary.json');
-    """)
-
     con.sql(
         f"""
         SELECT d.district, COUNT(*) AS num_places
